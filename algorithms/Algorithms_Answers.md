afa02c5d9ff864cebfef9aab721b332586d59e53
Daniel Plott <Daniel.P.Plott@gmail.com>
2018-05-11 16:47:27 -0500

    Completed

--- algorithms/Algorithms_Answers.md
+++ algorithms/Algorithms_Answers.md
@@ -1,1 +1,38 @@
 Add your answers to the Algorithms exercises here.
+
+Excercise 1 - 
+
+a) O(n)
+b) O(logn)
+c) O(sqrt (n))
+d) O(nlogn)
+e) O(n^3)
+f) O(n)
+g) O(n)
+
+Excercise 2 - 
+
+a)
+
+function maxDifference(arr) {
+    let minVal = arr[0];
+    let maxDiff = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        minVal = Math.min(minVal, arr[i]);
+        maxDiff = Math.max(maxDiff, arr[i] - minVal);
+    }
+
+    return maxDiff;
+}
+
+
+b) 
+
+Binary Search
+
+Excercise 3 -
+
+a) O(n^2) - the pivot is at one end of the sort
+
+b) the complexity would always be O(n log n) if the pivot were in the middle and successfully divided by 2
--- data_structures/src/heap.js
+++ data_structures/src/heap.js
@@ -1,63 +1,83 @@
 const heapsort = (arr) => {
   /* Your code here */
-  
+  // create a new heap
+  const heap = new Heap();
+
+  // loop through the arr and use the .insert method to put each element in the heap
+  arr.forEach(item => heap.insert(item));
+
+  // arr to hold the data once it is sorted
+  const sortedArr = [];
+
+  // while the heap's size is greater than 0 use .delete => which returns the highest number in the heap and reorder the heap to maintain its integrity
+  while (heap.getSize()) {
+    // take the value returned .delete and put it at the beginning of the sorted array
+    sortedArr.unshift(heap.delete());
+  }
+  // return the sorted array
+  return sortedArr;
 };
 
 
 class Heap {
   constructor() {
-    this.storage = [];
+    this.storage = [null];
+    this.size = 0;
+
   }
 
   insert(val) {
     const index = this.storage.push(val) - 1;
+    this.size++;
     this.bubbleUp(index);
   }
 
   delete() {
-    if (!this.storage.length) return null;
-    if (this.storage.length === 1) {
+    if (this.storage.length === 2) {
+      this.size--;
       return this.storage.pop();
+    } else if (this.storage.length === 1) {
+      return this.storage[0];
     }
-
-    const max = this.storage[0];
-    this.storage[0] = this.storage.pop();
-    this.siftDown(0);
+    this.size--;
+    const max = this.storage[1];
+    this.storage[1] = this.storage.pop();
+    this.siftDown(1);
     return max;
   }
 
   getMax() {
-    return this.storage[0];
+    return this.storage[1];
   }
 
   getSize() {
-    return this.storage.length;
+    return this.size;
   }
 
   bubbleUp(index) {
-    const parent = Math.floor((index - 1) / 2);
-    if (this.storage[parent] < this.storage[index]) {
+    const parent = Math.floor((index) / 2);
+    if (parent > 0 && this.storage[parent] < this.storage[index]) {
       [this.storage[parent], this.storage[index]] = [this.storage[index], this.storage[parent]];
       this.bubbleUp(parent);
     }
   }
 
   siftDown(index) {
-    const leftChildIndex = index * 2 + 1;
-    const rightChildIndex = index * 2 + 2;
+    const leftChildIndex = index * 2;
+    const rightChildIndex = index * 2 - 1;
     let maxChild;
     
-    if (this.storage[leftChildIndex] && this.storage[rightChildIndex]) {
-      maxChild = this.storage[leftChildIndex] > this.storage[rightChildIndex] ? leftChildIndex : rightChildIndex;
-    } else if (this.storage[leftChildIndex]) {
-      maxChild = leftChildIndex;
-    } else if (this.storage[rightChildIndex]) {
-      maxChild = rightChildIndex;
-    } 
+    if (this.storage[child1] !== undefined) {
+      if (this.storage[child2] === undefined) {
+        maxChild = child1;
+      } else if (this.storage[child2] !== undefined) {
+        maxChild = this.storage[child1] > this.storage[child2] ? child1 : child2;
+      }
 
-    if (this.storage[index] < this.storage[maxChild]) {
-      [this.storage[maxChild], this.storage[index]] = [this.storage[index], this.storage[maxChild]];
-      this.siftDown(maxChild);
+      if (this.storage[index] < this.storage[maxChild]) {
+        [this.storage[maxChild], this.storage[index]] = [this.storage[index], this.storage[maxChild]];
+        this.siftDown(maxChild);
+      }
     }
   }
 }
